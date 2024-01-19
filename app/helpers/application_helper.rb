56b334a21aa6ab998118fdf1b1ee02246c0d5d72
Matthew Blott <m4ttb@pm.me>
2024-01-19 21:44:03 +0000

    Working sign in form created

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def title
+    return t("gumtree-clone") unless content_for?(:title)
+    "#{content_for(:title)} | #{t("gumtree-clone")}"
+  end
+
 end
---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ApplicationRecord
+  before_validation :strip_extraneous_spaces
+
+  validates :name, presence: true
+  validates :email,
+            format: { with: URI::MailTo::EMAIL_REGEXP },
+            uniqueness: { case_sensitive: false }
+
+  has_many :memberships, dependent: :destroy
+  has_many :organizations, through: :memberships
+
+  private
+
+  def strip_extraneous_spaces
+    self.name = name&.strip
+    self.email = email&.strip
+  end
+
+  has_secure_password
+  validates :password,
+            presence: true,
+            length: { minimum: 8 }
+end
