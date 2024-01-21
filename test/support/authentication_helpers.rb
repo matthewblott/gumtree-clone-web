e0223c3b0b6af80104d54d6093512f4d5aaeb644
Matthew Blott <m4ttb@pm.me>
2024-01-21 20:07:58 +0000

    Test harness added for authentication tests

---
+++ test/support/authentication_helpers.rb
@@ -0,0 +1,13 @@
+module AuthenticationHelpers
+  def log_in(user, password: "password")
+    post(
+      login_path,
+      params: {
+        user: {
+          email: user.email,
+          password: password
+        }
+      }
+    )
+  end
+end
