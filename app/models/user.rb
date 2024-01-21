fc8e186f476685f181fc6a38c5314ad7f4284812
Matthew Blott <m4ttb@pm.me>
2024-01-21 20:24:44 +0000

    Refactored authentication code in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  include Authentication
+
   before_validation :strip_extraneous_spaces
 
   validates :name, presence: true
@@ -10,26 +12,6 @@ class User < ApplicationRecord
 
   has_many :memberships, dependent: :destroy
   has_many :organizations, through: :memberships
-  has_many :app_sessions
-
-  has_secure_password
-  validates(
-    :password,
-    presence: true,
-    length: {minimum: 8}
-  )
-
-  def self.create_app_session(email:, password:)
-    return nil unless user = User.find_by(email: email.downcase)
-
-    user.app_sessions.create if user.authenticate(password)
-  end
-
-  def authenticate_app_session(app_session_id, token)
-    app_sessions.find(app_session_id).authenticate_token(token)
-  rescue ActiveRecord::RecordNotFound
-    nil
-  end
 
   private
 
