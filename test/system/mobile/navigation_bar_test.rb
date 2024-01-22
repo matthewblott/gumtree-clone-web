b66c9b49da83377aeec2b5221f3c17b6defa0b75
Matthew Blott <m4ttb@pm.me>
2024-01-22 13:07:40 +0000

    Fixed issue with hamburger menu test

--- test/system/mobile/navigation_bar_test.rb
+++ test/system/mobile/navigation_bar_test.rb
@@ -10,7 +10,8 @@ module Mobile
     end
 
     test("can access login page via burger menu") do
-      visit root_path find(".navbar-burger").click
+      visit root_path
+      find(".navbar-burger").click
       click_on I18n.t("shared.navbar.login")
       assert_current_path login_path
     end
